35f9a455346f0dfdc9f5b804b78f7a7afd761fd5
Liam Wilford <lwilf039@uottawa.ca>
2018-10-24 15:53:39 -0400

    Added all activities for deliverable 1

---
+++ Android App/app/src/main/java/com/example/liamwilford/myapplication/LogInScreenActivity.java
@@ -0,0 +1,13 @@
+package com.example.liamwilford.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LogInScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_in_screen);
+    }
+}
