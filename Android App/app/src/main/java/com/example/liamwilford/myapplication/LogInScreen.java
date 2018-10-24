4086eacde84c513ad69383e9391486ef16b9508e
NGnius <ngniusness@gmail.com>
2018-10-24 17:10:29 -0400

    Add login verification

--- Android App/app/src/main/java/com/example/liamwilford/myapplication/LogInScreen.java
+++ Android App/app/src/main/java/com/example/liamwilford/myapplication/LogInScreen.java
@@ -2,6 +2,9 @@ package com.example.liamwilford.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.regex.Pattern;
 
 public class LogInScreen extends AppCompatActivity {
 
@@ -10,4 +13,34 @@ public class LogInScreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in_screen);
     }
+
+    /**
+     * Validate the username in Sign Up activity
+     * Currently checks for empty and spaces
+     *
+     * @return the validity of the username (true if valid, false if invalid)
+     */
+    protected boolean validateUsername(){
+        String pattern = "[^\s]*";
+        TextView usernameView = (TextView) findViewById(R.id.logInUserNameEditText);
+        String username = (String) usernameView.getText();
+
+        return (Pattern.matches(pattern, username) && !username.equals(""));
+    }
+
+    /**
+     * Valid the password in Sign Up activity
+     * Currently checks for empty and spaces
+     *
+     * @return the validity of the username (true if valid, false if invalid)
+     */
+    protected boolean validatePassword(){
+        String pattern = "[^\s]*";
+        TextView passwordView = (TextView) findViewById(R.id.editText2);
+        String password = (String) passwordView.getText();
+
+        return (Pattern.matches(pattern, password) && !password.equals(""));
+    }
+    // TODO: Check if username actually exists in db
+    // TODO: Check if password matches with password in db
 }
