9ca516f5131c6549a318fead02bad2119e0deff1
NGnius <ngniusness@gmail.com>
2018-10-25 16:42:26 -0400

    Add email validation and fix regex

--- Android App/app/src/main/java/com/example/liamwilford/myapplication/SignUpActivity.java
+++ Android App/app/src/main/java/com/example/liamwilford/myapplication/SignUpActivity.java
@@ -20,25 +20,41 @@ public class SignUpActivity extends AppCompatActivity {
      * @return the validity of the username (true if valid, false if invalid)
      */
     protected boolean validateUsername(){
-        String pattern = "[^\s]*";
+        String pattern = "[^\\s]*";
         TextView usernameView = (TextView) findViewById(R.id.logInUserNameEditText);
         String username = (String) usernameView.getText();
 
-        return (Pattern.matches(pattern, username) && !username.equals(""));
+        return ( !username.equals("") && Pattern.matches(pattern, username) );
     }
 
     /**
-     * Valid the password in Sign Up activity
+     * Validate the password in Sign Up activity
      * Currently checks for empty and spaces
      *
-     * @return the validity of the username (true if valid, false if invalid)
+     * @return the validity of the password (true if valid, false if invalid)
      */
     protected boolean validatePassword(){
-        String pattern = "[^\s]*";
+        String pattern = "[^\\s]*";
         TextView passwordView = (TextView) findViewById(R.id.editText2);
         String password = (String) passwordView.getText();
 
-        return (Pattern.matches(pattern, password) && !password.equals(""));
+        return ( !password.equals("") && Pattern.matches(pattern, password));
     }
     //TODO: Check if username already exists in db
+
+    /**
+     * Validate email
+     * Currently checks for empty and email of form [name]@[site].[ending]
+     *
+     * @return the validity of the email (true if valid, false if invalid)
+     */
+    protected boolean validateEmail(){
+        //TODO: add email input field
+        String pattern = "[^\\@\\s]+\\@[^\\.^\\s]+\\.[\\S]+";
+        TextView emailView = (TextView) findViewById(R.id.editText2); //TODO: use email field
+        String email = (String) emailView.getText();
+
+        return ( !email.equals("") && Pattern.matches(pattern, email) );
+    }
+
 }
