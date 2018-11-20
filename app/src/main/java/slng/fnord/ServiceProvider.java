e954a26b35f0797b6b93895074bea8d0c622c085
Natalia Maximo <bprad071@uottawa.ca>
2018-11-20 13:14:50 -0500

    Added nested class to ServiceProvider to hold service metadata

--- app/src/main/java/slng/fnord/ServiceProvider.java
+++ app/src/main/java/slng/fnord/ServiceProvider.java
@@ -4,14 +4,52 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class ServiceProvider extends User {
+    private class SPService {
+        private String name;
+        private boolean isCertified;
+
+        private SPService() {
+
+        }
+
+        private SPService(String name, boolean certified) {
+            this.name = name;
+            this.isCertified = certified;
+        }
+
+        public boolean isEqual(SPService other) {
+            return other.name.equalsIgnoreCase(this.name) && this.isCertified == other.isCertified;
+        }
+
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public boolean isCertified() {
+            return isCertified;
+        }
+
+        public void setCertified(boolean certified) {
+            isCertified = certified;
+        }
+    }
+
+
+    private List<SPService> services;
+    private String phone;
+    private String address;
+
+
+
+    // Necessary for DB
     public ServiceProvider() {
 
     }
-    private List<String> services;
-    private String SPAddress;
-    private String SPPhoneNumber;
-    private String SPCompanyName;
-    private String SPBiography;
 
     public ServiceProvider(String email, String username, String password) {
         super(email, username, password, UserTypes.SERVICEPROVIDER);
@@ -22,19 +60,15 @@ public class ServiceProvider extends User {
         SPBiography = "";
     }
 
-    public void addService(String serviceName) {
-        services.add(serviceName);
+    public void addService(String serviceName, boolean certified) {
+        services.add(new SPService(serviceName, certified));
     }
 
     public void removeService(String serviceName) {
         services.remove(serviceName);
     }
 
-    public void setServiceList(List<String> serviceList) {
-        this.services = serviceList;
-    }
-
-    public List<String> getServiceList() {
+    public List<SPService> getServiceList() {
         return services;
     }
 
