cb4deda9aa49c7f646f213c90f41111a591fda5e
NGnius <ngniusness@gmail.com>
2018-11-20 12:51:18 -0500

    Make availability by weekday and remove activity

--- app/src/main/java/slng/fnord/Availability.java
+++ app/src/main/java/slng/fnord/Availability.java
@@ -1,52 +1,17 @@
 package slng.fnord;
 
-import android.content.ContentUris;
-import android.content.Intent;
-import android.net.Uri;
-import android.provider.CalendarContract.Events;
-import android.provider.*;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.CalendarView;
-
 import java.util.HashMap;
 
-public class Availability extends AppCompatActivity {
-    private CalendarView cal = null;
+public class Availability {
 
-    private HashMap<Long, AvailabilityEnum[]> savedSlots= new HashMap<Long, AvailabilityEnum[]>();
+    private HashMap<WeekDay, AvailabilityEnum[]> savedSlots= new HashMap<WeekDay, AvailabilityEnum[]>();
+    private WeekDay[] weekDays = new WeekDay[]{WeekDay.SUNDAY, WeekDay.MONDAY, WeekDay.TUESDAY, WeekDay.WEDNESDAY, WeekDay.THURSDAY, WeekDay.FRIDAY, WeekDay.SATURDAY};
 
-    private AvailabilityEnum[] timeSlots;
-    private long lastDate;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_availability);
-
-        timeSlots = new AvailabilityEnum[24];
-
-        cal = (CalendarView) findViewById(R.id.availabilityCal);
-        lastDate = cal.getDate();
-        cal.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
-            @Override
-            public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMonth) {
-                long newDate = cal.getDate();
-                saveTimeSlots(lastDate, timeSlots);
-                // create/load timeslots for day
-                if (savedSlots.containsKey(newDate) ){
-                    timeSlots = savedSlots.get(newDate);
-                } else {
-                    timeSlots = new AvailabilityEnum[24];
-                }
-                lastDate = newDate;
-
-            }
-        });
+    protected Availability() {
+        initTimeSlots();
     }
 
-    public void openCalendar(View v){
+    /*public void openCalendar(View v){
 
         Uri.Builder builder = CalendarContract.CONTENT_URI.buildUpon();
         builder.appendPath("time");
@@ -54,19 +19,36 @@ public class Availability extends AppCompatActivity {
         Intent intent = new Intent(Intent.ACTION_VIEW)
                 .setData(builder.build());
         startActivity(intent);
-    }
+    }*/
 
-    public void setSlotAvailablity(int index, AvailabilityEnum avail){
+    public void setSlotAvailablity(int index, WeekDay day, AvailabilityEnum avail){
+        AvailabilityEnum[] timeSlots = getTimeSlots(day);
         timeSlots[index]=avail;
+        saveTimeSlots(day, timeSlots);
     }
 
     //TODO: method(s) to set slot availability from UI
 
-    private void saveTimeSlots(long date, AvailabilityEnum[] times){
+    private void saveTimeSlots(WeekDay day, AvailabilityEnum[] times){
         // TODO: Backup to db
         // times is an array of 24 slots (1 hour each), where true means available
         // date is a time since the epoch, in milliseconds
-        savedSlots.put(date, times);
+        savedSlots.put(day, times);
 
+    }
+
+    private void initTimeSlots(){
+        // init savedSlots; sets all time slots to AVAILABLE
+        for (int dayIndex=0; dayIndex<weekDays.length; dayIndex++){
+            AvailabilityEnum[] slots = new AvailabilityEnum[24];
+            for (int i=0; i<24; i++){
+                slots[i]=AvailabilityEnum.AVAILABLE;
+            }
+            saveTimeSlots(weekDays[dayIndex], slots);
+        }
+    }
+
+    public AvailabilityEnum[] getTimeSlots(WeekDay day){
+        return savedSlots.get(day);
     }
 }
