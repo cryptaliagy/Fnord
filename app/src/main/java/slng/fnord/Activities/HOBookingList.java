01e6374a17adf105d09c02c1896172a2ca0cde42
Liam Wilford <kiersonliam@gmail.com>
2018-11-28 22:28:40 -0500

    Added Proposed Activities
    These include: pages for home owner: search page, search results, service profile page,Book service Page, bookings list, review booking page

---
+++ app/src/main/java/slng/fnord/Activities/HOBookingList.java
@@ -0,0 +1,16 @@
+package slng.fnord.Activities;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+import slng.fnord.R;
+
+public class HOBookingList extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hobooking_list);
+    }
+
+}
---
+++ app/src/main/java/slng/fnord/Activities/HOsearchResults.java
@@ -0,0 +1,16 @@
+package slng.fnord.Activities;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+import slng.fnord.R;
+
+public class HOsearchResults extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hosearch_results);
+    }
+
+}
