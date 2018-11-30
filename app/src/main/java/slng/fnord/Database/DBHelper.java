fb4fcecab2d5b1ba272573f8cdefcd4b9164743e
Natalia Maximo <bprad071@uottawa.ca>
2018-11-30 00:11:06 -0500

    Migrated minimum API to level 24 and added DB helper method
    New DB helper method allows you to search for specific data in the DB and pass that to a callback that you set. It's actually really neat

--- app/src/main/java/slng/fnord/Database/DBHelper.java
+++ app/src/main/java/slng/fnord/Database/DBHelper.java
@@ -2,8 +2,6 @@ package slng.fnord.Database;
 
 import android.support.annotation.NonNull;
 
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -12,59 +10,42 @@ import com.google.firebase.database.ValueEventListener;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Consumer;
 
 import io.reactivex.Completable;
-import io.reactivex.CompletableEmitter;
-import io.reactivex.CompletableOnSubscribe;
 import io.reactivex.Observable;
-import io.reactivex.ObservableEmitter;
-import io.reactivex.ObservableOnSubscribe;
 import slng.fnord.Helpers.Common;
 import slng.fnord.Structures.Services;
 import slng.fnord.Structures.User;
 
 public class DBHelper {
     public static Observable<DataSnapshot> makeObservableFromPath(final String path) {
-        return Observable.create(new ObservableOnSubscribe<DataSnapshot>() {
-            @Override
-            public void subscribe(final ObservableEmitter<DataSnapshot> emitter) {
-                DatabaseReference ref = FirebaseDatabase.getInstance().getReference(path);
-                ValueEventListener listener = new ValueEventListener() {
-                    @Override
-                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                        emitter.onNext(dataSnapshot);
-                        emitter.onComplete();
-                    }
+        return Observable.create(emitter -> {
+            DatabaseReference ref = FirebaseDatabase.getInstance().getReference(path);
+            ValueEventListener listener = new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                    emitter.onNext(dataSnapshot);
+                    emitter.onComplete();
+                }
 
-                    @Override
-                    public void onCancelled(@NonNull DatabaseError databaseError) {
-                        emitter.onError(databaseError.toException());
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
+                    emitter.onError(databaseError.toException());
 
-                    }
-                };
+                }
+            };
 
-                ref.addListenerForSingleValueEvent(listener);
-            }
+            ref.addListenerForSingleValueEvent(listener);
         });
     }
 
     public static Completable makeCompletableFromPath(final String path, final Object value) {
-        return Completable.create(new CompletableOnSubscribe() {
-            @Override
-            public void subscribe(final CompletableEmitter emitter) throws Exception {
-                DatabaseReference ref = FirebaseDatabase.getInstance().getReference(path);
-                ref.setValue(value).addOnSuccessListener(new OnSuccessListener<Void>() {
-                    @Override
-                    public void onSuccess(Void aVoid) {
-                        emitter.onComplete();
-                    }
-                }).addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        emitter.onError(e);
-                    }
-                });
-            }
+        return Completable.create(emitter -> {
+            DatabaseReference ref = FirebaseDatabase.getInstance().getReference(path);
+            ref.setValue(value)
+                    .addOnSuccessListener(e -> emitter.onComplete())
+                    .addOnFailureListener(emitter::onError);
         });
     }
 
@@ -92,6 +73,28 @@ public class DBHelper {
     public static void deleteService(String service) {
         String id = Common.makeMD5(service);
 
-        FirebaseDatabase.getInstance().getReference("services/"+id).removeValue();
+        FirebaseDatabase.getInstance().getReference("services/" + id).removeValue();
+    }
+
+    public static <T> void getData(String type, String id, Class<T> dataClass, Consumer<T> callback) {
+        DatabaseReference dbRef = FirebaseDatabase.getInstance().getReference(type + "/" + id);
+
+        dbRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if (!dataSnapshot.exists()) {
+                    callback.accept(null);
+                }
+                else {
+                    T data = dataSnapshot.getValue(dataClass);
+                    callback.accept(data);
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                callback.accept(null);
+            }
+        });
     }
 }
